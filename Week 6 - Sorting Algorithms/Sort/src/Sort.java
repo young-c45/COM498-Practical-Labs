63ce4376e1cba632cdb8fe5433ff0d556b70c869
Callum Young <young-c45@ulster.ac.uk>
2022-11-08 14:46:50 +0000

    Scenario 6.6

--- Week 6 - Sorting Algorithms/Sort/src/Sort.java
+++ Week 6 - Sorting Algorithms/Sort/src/Sort.java
@@ -6,6 +6,7 @@
  */
 
 import java.util.Arrays;
+import java.util.Random;
 
 // Template for Sort objects
 public class Sort {
@@ -25,20 +26,18 @@ public class Sort {
             // Runs for every unsorted item in array
             for (int j = 0; j < innerLastPos; j++) {
                 // Stores that a comparison was made
-                numComparisons++;
+                //numComparisons++;
                 // Swaps value at j if more than next value
                 if (arr[j] > arr[j+1]) {
                     temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                     // Stores that a swap was made
-                    numSwaps++;
+                    //numSwaps++;
                 }
             }
             // Shrinks inner area to exclude sorted elements
             innerLastPos--;
-            // Outputs information about the pass
-            System.out.println("Pass " + (i+1) + ": " + Arrays.toString(arr));
             
         }
     }
@@ -56,42 +55,69 @@ public class Sort {
             // Runs for every unsorted item in array
             for (int j = firstPos; j < lastPos; j++) {
                 // Stores that a comparison was made
-                numComparisons++;
+                //numComparisons++;
                 // Swaps value at j if more than next value
                 if (arr[j] > arr[j+1]) {
                     temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                     // Stores that a swap was made
-                    numSwaps++;
+                    //numSwaps++;
                     // Sets the last swapped position to this position
                     lastSwapPos = j;
                 }
             }
             // Shrinks inner area to exclude sorted elements
             lastPos = lastSwapPos;
-            // Outputs information about the pass
-            System.out.println("Pass: " + Arrays.toString(arr));
 
         }
+    }
+    
+    // Method to generate a random integer array
+    public static int[] randomArray(int n) {
+        // Creates object to generate random numbers
+        Random random = new Random();
+        // Declares array of size n
+        int[] arr = new int[n];
+        
+        // Fills array with random numbers
+        for (int i = 0; i < n; i++) {
+            arr[i] = random.nextInt(n);
+        }
+        
+        // Returns array
+        return arr;
     }
 
     // Method to run on compile
     public static void main(String[] args) {
-        // Creates array to sort
-        int[] arr = { 9, 2, 7, 1, 10, 3, 6, 4, 5, 8 };
-        // Outputs array
-        System.out.println("Before: " + Arrays.toString(arr));
-        // Outputs line for formatting
-        System.out.println();
+        // Declares timing variables
+        long startTime, endTime;
+        // Declares array to sort
+        int[] arr;
         
-        // Sorts array
-        bubblesort(arr);
-        // Outputs result
-        System.out.println("\nAfter: " + Arrays.toString(arr));
-        
-        // Outputs statistics
-        System.out.println("\nThere were " + numComparisons 
-                + " comparisons and " + numSwaps + " swaps.");
+        // Gets the starting time
+        startTime = System.currentTimeMillis();
+        // Sorts 1000 random arrays
+        for (int i = 0; i < 1000; i++) {
+            arr = randomArray(100);
+            bubblesort(arr);
+        }
+        // Gets the ending time
+        endTime = System.currentTimeMillis();
+        // Outputs time taken
+        System.out.println("Execution time is " + (endTime - startTime));
+
+        // Gets the starting time
+        startTime = System.currentTimeMillis();
+        // Sorts 1000 random arrays
+        for (int i = 0; i < 1000; i++) {
+            arr = randomArray(100);
+            bubblesort_slow(arr);
+        }
+        // Gets the ending time
+        endTime = System.currentTimeMillis();
+        // Outputs time taken
+        System.out.println("Execution time is " + (endTime - startTime));
     }
 }
